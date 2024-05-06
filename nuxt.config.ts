6fde8116bfadc2cc732e2d6bddacc3edcc2d4d42
joannaberlin <joanna.kot.ok@gmail.com>
2024-05-06 16:57:27 +0200

    Add meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'Joanna Did This',
+			charset: 'utf-16',
+			viewport: 'width=device-width, initial-scale=1',
+		},
+	},
 	modules: ['@nuxtjs/tailwindcss'],
 });
