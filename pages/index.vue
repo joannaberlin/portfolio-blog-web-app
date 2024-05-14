917240d241ec1606265a7d805655a20bcc748249
joannaberlin <joanna.kot.ok@gmail.com>
2024-05-14 15:11:35 +0200

    Add image and styles to home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,32 @@
 <script lang="ts" setup></script>
 <template>
-	<h1>Index File</h1>
+	<main>
+		<section class="hero bg-gray-100 py-10">
+			<div class="container flex">
+				<div class="container flex min-h-[200px]">
+					<div class="w-[350px] relative h-[auto] mr-5 rounded overflow-hidden">
+						<img
+							src="/assets/images/jo-poster-frame.jpg"
+							alt="Poster graphic of the blog's author."
+							class="absolute object-cover shadow-md"
+						/>
+					</div>
+					<div>
+						<h1 class="text-4xl font-bold mb-2">About Joanna</h1>
+						<p class="mb-3">
+							Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+							Reiciendis aspernatur tenetur cupiditate laboriosam. Tempore
+							dolorum accusamus aperiam quaerat dicta, veniam officia quisquam
+							omnis ab, doloribus necessitatibus quod eligendi ad eveniet.
+						</p>
+						<a
+							class="btn bg-sky-500 text-sky-50 py-2 px-4 rounded inline-block hover:bg-sky-600 duration-200"
+							href="*"
+							>Let's connect!</a
+						>
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
 </template>
